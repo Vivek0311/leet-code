1d3764091aaaf9cf19016e3b9f69ac3f252cd8d1
Vivek Sirwal <86698149+Vivek0311@users.noreply.github.com>
2025-02-25 22:13:21 +0530

    1768. Merge Strings Alternately

---
+++ 1768. Merge Strings Alternately.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0;
+        int j = 0;
+        string merged = "";
+        while(i < word1.length() && j < word2.length()){
+            merged = merged + word1[i] + word2[j];
+            i++;
+            j++;
+        }
+        if ( i < word1.length()){
+            merged = merged + word1.substr(i, word1.length());
+        }
+        if ( j < word2.length()){
+            merged = merged + word2.substr(j, word2.length());
+        }
+        return merged;
+    }
+};
