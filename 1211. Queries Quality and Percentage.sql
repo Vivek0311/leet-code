53a56205fab028220a16981a2935981b4028de44
Vivek Sirwal <86698149+Vivek0311@users.noreply.github.com>
2025-12-25 19:11:59 +0530

    1211. Queries Quality and Percentage

---
+++ 1211. Queries Quality and Percentage.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+select query_name, round(sum(rating / position) / count(position), 2) as quality, round((count(case when rating < 3 then 1 end) / count(rating)) * 100, 2) as poor_query_percentage
+from Queries
+group by query_name
