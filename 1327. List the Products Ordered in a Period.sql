952df866fe4ae68c0d8ccffb3798e9d984e61605
Vivek Sirwal <86698149+Vivek0311@users.noreply.github.com>
2025-12-25 19:41:11 +0530

    1327. List the Products Ordered in a Period

---
+++ 1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,7 @@
+select p.product_name, sum(unit) as unit
+from Orders o
+join Products p 
+on o.product_id = p.product_id
+where order_date between '2020-02-01' and '2020-02-29'
+group by p.product_name
+having sum(unit) >= 100
