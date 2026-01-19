31281f8280e0c618048c186f3ab884c768ad8144
Vivek Sirwal <86698149+Vivek0311@users.noreply.github.com>
2026-01-19 21:46:50 +0530

    3497. Analyze Subscription Conversion

---
+++ 3497. Analyze Subscription Conversion.sql
@@ -0,0 +1,8 @@
+select user_id, 
+round(avg(case when activity_type = 'free_trial' then activity_duration end), 2) as trial_avg_duration,
+round(avg(case when activity_type = 'paid' then activity_duration end), 2) as paid_avg_duration
+from UserActivity
+where activity_type in ('free_trial', 'paid')
+group by user_id
+having count(distinct activity_type) = 2
+order by user_id;
